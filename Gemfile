e5d685db4cac2ffa3070b89e8572c2803254cf15
Piotr Nowacki <p.nowacki90@gmail.com>
2018-01-25 21:43:23 +0000

    Back to rails. Integration test finished, minitest gem fixed, recipes index in progress

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'minitest', '5.10.3'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'sqlite3'
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  def setup
+    @user = Chef.create!(chefname: "piotrek", email: "wpssss@wp.pl")
+    @recipe = Recipe.create(name: "veggie burger", description: "Dziwny burger", chef_id: @chef)
+    @recipe2 = @user.recipes.build(name: "pesto", description: "bazylia i parmezan")
+    @recipe2.save
+  end
+  
+  test "should get recipes index" do
+    get recipes_url
+    assert_response :success
+  end
+  
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body
+    assert_match @recipe2.name, response.body
+    
+  end
+end
