2ef4005bd77476d9fdb98867262187b48cdfb1e9
Piotr Nowacki <p.nowacki90@gmail.com>
2018-01-28 11:44:46 +0000

    Recipes_test updated, minor custom.css changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,13 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
