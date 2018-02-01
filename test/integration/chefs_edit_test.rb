286b1e4c2f41b9525d1ac7b19dd365f0dba84a14
Piotr Nowacki <p.nowacki90@gmail.com>
2018-02-01 21:26:02 +0000

    Added edit action

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = Chef.create!(chefname: "piotrek", email: "wpssss@wp.pl",
+                        password: "password", password_confirmation: "password")
+  end
+  
+  test "Reject an invalid edit" do
+    get edit_chef_path(@user)
+    assert_template "chefs/edit"
+    patch chef_path(@user), params: { chef: { chefname: "", email: "kjbsadkjb@wp.pl" } }
+    assert_template 'chefs/edit'
+    assert_select "h2.panel-title"
+    assert_select "div.panel-body"
+  end
+  
+  test "accept valid signup" do
+    get edit_chef_path(@user)
+    assert_template "chefs/edit"
+    patch chef_path(@user), params: { chef: { chefname: "klekotek", email: "kjbsadksjb@wp.pl" } }
+    assert_redirected_to @user
+    assert_not flash.empty?
+    @user.reload
+    assert_match "klekotek", @user.chefname
+    assert_match "kjbsadksjb@wp.pl", @user.email
+  end
+end
