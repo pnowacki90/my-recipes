5b83dbadf2ca7829032c1bc3fe2e5ac46f47e97e
Piotr Nowacki <p.nowacki90@gmail.com>
2018-02-02 12:03:54 +0000

    Added chefs listing and tests

---
+++ test/integration/chefs_listing_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ChefsListingTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = Chef.create!(chefname: "piotrek", email: "wpssss@wp.pl",
+                        password: "password", password_confirmation: "password")
+    @user2 = Chef.create!(chefname: "robert", email: "wpsssrs@wsp.pl",
+                        password: "1password", password_confirmation: "1password")
+  end
+  
+  test "should get chefs index" do
+    get chefs_url
+    assert_response :success
+  end
+  
+  test "should get chefs listing" do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_select "a[href=?]", chef_path(@user), text: @user.chefname.capitalize
+    assert_select "a[href=?]", chef_path(@user2), text: @user2.chefname.capitalize
+  end
+end
