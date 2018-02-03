732fb7dbbc4a616bab890660826c130eceb75f40
Piotr Nowacki <p.nowacki90@gmail.com>
2018-02-03 17:13:08 +0000

    Added delete function for chefs and associated recipes

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -37,6 +37,13 @@ class ChefsController < ApplicationController
     end
   end
   
+  def destroy
+    @chef = Chef.find(params[:id])
+    @chef.destroy
+    flash[:danger] = "Chef and all his/her recipes were deleted"
+    redirect_to chefs_path
+  end
+  
   private
   
   def chef_params
